48612bc51768973a29ab01029453ece9a92ceb27
Ese-oghene <oghenee08@gmail.com>
2024-12-19 11:14:25 +0100

    image-amount

--- src/app/product-grid/img-amtbutons/img-amtbutons.component.ts
+++ src/app/product-grid/img-amtbutons/img-amtbutons.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  title: string;
+  price: number;
+  imageUrl: string;
+  isNew: boolean;
+  isOnSale: boolean;
+}
 
 @Component({
   selector: 'app-img-amtbutons',
@@ -7,7 +14,29 @@ import { Component } from '@angular/core';
   styleUrl: './img-amtbutons.component.css'
 })
 export class ImgAmtbutonsComponent  {
- 
+  products: Product[] = [
+    {
+      title: 'Gown',
+      price: 1000,
+      imageUrl: '../images/clothes/p3.png',
+      isNew: true,
+      isOnSale: true
+    },
+    {
+      title: 'Long Leave',
+      price: 1000,
+      imageUrl: '../images/clothes/p1.jpeg',
+      isNew: true,
+      isOnSale: true
+    },
+    {
+      title: 'HandLess Gown',
+      price: 1000,
+      imageUrl: '../images/clothes/p4.png',
+      isNew: true,
+      isOnSale: true
+    }
+  ];
 
 
 }
